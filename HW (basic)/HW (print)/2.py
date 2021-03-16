5a75183f90d9a53a13d75290cb446e7ea535dbe6
zigolade <80662640+zigolade@users.noreply.github.com>
2021-03-16 17:33:26 +0900

    Add files via upload

---
+++ HW (basic)/HW (print)/2.py
@@ -0,0 +1,9 @@
+# 다음과 같이 출력되는 프로그램을 작성하라.
+# 각 요소사이의 공백은 tab으로 구분.
+
+# tem count price
+# pen 20 100
+# note 5 95
+# eraser 110 97
+
+print("item\tcount\tprice\npen\t20\t100\nnote\t5\t95\neraser\t110\t97")
